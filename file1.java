8f9b08976fa8c5a68babaa41a2e3e52d1d7eedec
roopesh777 <82398377+roopesh777@users.noreply.github.com>
2024-12-20 20:13:04 +0530

    Update file1.java

--- file1.java
+++ file1.java
@@ -2,3 +2,7 @@ Hi I am learning devops from Frontlines Media
 I am new to this topic
 I love playing cricket
 I love studying more and more.
+
+I love studying and gaining the hands on experience
+This is one of the most important thing in my life
+  
